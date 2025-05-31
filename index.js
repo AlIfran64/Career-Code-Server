f76bb1103edcf91aa299ecea07cd356c9cd6a8b0
Al Ifran <ifran15-2915@diu.edu.bd>
2025-05-31 23:41:08 +0600

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Mongo Connection
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lds4lih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const careerCollection = client.db("careerDb").collection("career");
+
+    // GET
+    app.get("/careers", async (req, res) => {
+      const result = await careerCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Specific GET
+    app.get("/careers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await careerCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
