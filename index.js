6854251b20aa882147b2b397a758629e90b11100
Al Ifran <ifran15-2915@diu.edu.bd>
2025-06-13 09:57:25 +0600

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-adminsdk.json");
 
 // Middleware
 app.use(cors());
@@ -21,16 +23,54 @@ const client = new MongoClient(uri, {
   },
 });
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).send({ error: "Unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.decoded = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).send({ error: "Forbidden access" });
+  }
+};
+
+const verifyTokenEmail = async (req, res, next) => {
+  if (req.quer.email !== req.decoded.email) {
+    return res.status(403).send({ error: "Forbidden access" });
+  }
+  next();
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const careerCollection = client.db("careerDb").collection("career");
+    const applicationCollection = client
+      .db("careerDb")
+      .collection("applications");
 
+    // Jobs api
     // GET
     app.get("/careers", async (req, res) => {
-      const result = await careerCollection.find().toArray();
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.hr_email = email;
+      }
+      const result = await careerCollection.find(query).toArray();
       res.send(result);
     });
 
@@ -39,6 +79,70 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await careerCollection.findOne(query);
+      res.send(result);
+    });
+
+    // POST
+    app.post("/careers", async (req, res) => {
+      const newJob = req.body;
+      const result = await careerCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    // Job application related api's
+
+    //GET
+    app.get("/applications", verifyFirebaseToken, async (req, res) => {
+      const email = req.query.email;
+
+      // if (email !== req.decoded.email) {
+      //   return res.status(403).send({ error: "Forbidden access" });
+      // }
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ error: "Forbidden access" });
+      }
+
+      const query = { applicant: email };
+      const result = await applicationCollection.find(query).toArray();
+
+      // Bad way to aggregate data
+      for (const application of result) {
+        const id = application.id;
+        const jobQuery = { _id: new ObjectId(id) };
+        const job = await careerCollection.findOne(jobQuery);
+        application.company = job.company;
+        application.title = job.title;
+        application.company_logo = job.company_logo;
+      }
+
+      res.send(result);
+    });
+
+    app.get("/applications/job/:id", async (req, res) => {
+      const jobId = req.params.id;
+      const query = { id: jobId };
+      const result = await applicationCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // POST
+    app.post("/applications", async (req, res) => {
+      const application = req.body;
+      const result = await applicationCollection.insertOne(application);
+      res.send(result);
+    });
+
+    // PATCH
+    app.patch("/application/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: req.body.status,
+        },
+      };
+      const result = await applicationCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
 
